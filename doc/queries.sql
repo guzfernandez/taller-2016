7a578820a284f49b76eec1252b4cf3853105469d
RodrigoArangoHub <rodrigo.armen@hotmail.com>
2016-07-26 20:59:13 -0300

    Select
    con las cordes para que salgan los dos puntos de la bd

--- doc/queries.sql
+++ doc/queries.sql
@@ -1,6 +1,6 @@
 SELECT * FROM points as p
   WHERE ST_DWithin(
     p.location,
-    Geography(ST_MakePoint(-72.060316, 48.432044)),
-    100
+    Geography(ST_MakePoint(-35.060316, -55.432044)),
+    60000
   );
